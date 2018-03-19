ef8e6741e80a27ed1bb9e08b4dd0ed23e1b64f2a
Djordje Maric <djordje.maric@quantox.com>
2018-03-19 12:11:15 +0100

    Seeder Added

--- app/User.php
+++ app/User.php
@@ -36,7 +36,7 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this->hasMany(Post::class)->scopeLatestFirst();
+        return $this->hasMany(Post::class);
     }
 
     public function comments()
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,11 +13,10 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
-        'username' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'title' => $faker->realText(30),
+        'body' => $faker->realText(250),
+        'user_id' => factory(App\User::class),
     ];
 });
